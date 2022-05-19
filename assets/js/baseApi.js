906bbb4fe93247b982839deb437ee66cfdd3f46c
liujianghao <1477166497@qq.com>
2022-05-19 14:06:09 +0800

    完成了主页功能的开发

--- assets/js/baseApi.js
+++ assets/js/baseApi.js
@@ -3,5 +3,20 @@
 // 我们给ajax发的配置对象
 $.ajaxPrefilter(function(options){
     options.url='http://www.liulongbin.top:3007'+options.url
-    console.log(options.url);
+    // console.log(options.url);
+    if(options.url.indexOf('/my/')!==-1){
+        options.headers={
+            Authorization:localStorage.getItem('token')||''
+        }
+    }
+    options.complete=function(res){
+        console.log(res);
+        //在complete回调函数中 会使用res.responseJSON拿到服务器响应的数据
+        if(res.responseJSON.status===1 && res.responseJSON.message==='身份认证失败！'){
+        //强制清空token
+        localStorage.removeItem('token')
+        //强制跳转登录页面
+        location.href='/login.html'
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,56 @@
+$(function(){
+    getUserInfo()
+    var layer=layui.layer
+    $('#btnLogout').on('click',function(){
+        // console.log('ok');
+        // 提示用户是否确认退出
+        layer.confirm('确定退出登录?', {icon: 3, title:'提示'}, function(index){
+            //do something
+            //1.清除本地存储的token
+        localStorage.removeItem('token')
+            //2.跳转至登录首页
+        location.href='/login.html'
+        // 这是关闭询问框
+        layer.close(index);
+    })
+})
+})
+function getUserInfo(){
+    $.ajax({
+        method:"GET",
+        url:'/my/userinfo',
+        //headers是请求头配置对象
+        // headers:
+        // {Authorization:localStorage.getItem('token')||''},
+        success:function(res){
+            console.log(res);
+            if(res.status!==0){
+                return layui.layer.msg('获取用户信息失败')
+            }
+            renderAvatar(res.data)
+        },
+        // complete:function(res){
+        //     console.log(res);
+        //     //在complete回调函数中 会使用res.responseJSON拿到服务器响应的数据
+        //     if(res.responseJSON.status===1 && res.responseJSON.message==='身份认证失败！'){
+        //     //强制清空token
+        //     localStorage.removeItem('token')
+        //     //强制跳转登录页面
+        //     location.href='/login.html'
+        //     }
+        // }
+    })
+}
+function renderAvatar(user){
+    var name=user.nickname||user.username
+    $('#welcome').html('欢迎&nbsp'+name)
+    if(user.user_pic!==null){
+        $('.layui-nav-img').attr('src',user.user_pic).show()
+        $('.textavatar').hide()
+    }
+    else{
+        var first=name[0].toUpperCase()
+        $('.textavatar').html(first).show()
+        $('.layui-nav-img').hide()
+    }
+}
